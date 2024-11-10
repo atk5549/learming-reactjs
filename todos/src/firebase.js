91607d8fa5580365ad02ccf681f40f38245fd406
KovalchukR_HomePC1 <atk5549@yandex.ru>
2024-11-10 13:54:20 +1000

    add firebaseConfig via npm with realtimeDatabase on firebase.google.com

---
+++ todos/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDapeBtrw78QgTp8Nav8Zm4igfIlzt-1bE",
+    authDomain: "todos-35ab9.firebaseapp.com",
+    projectId: "todos-35ab9",
+    storageBucket: "todos-35ab9.firebasestorage.app",
+    messagingSenderId: "686060997568",
+    appId: "1:686060997568:web:29434ec4527b0281b2d625"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
